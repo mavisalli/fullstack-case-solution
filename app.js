986f88ac58d9e05c07d581c2f8945cf033fe4ec5
mavi <mavi.baris96@gmail.com>
2022-02-07 04:57:06 +0300

    Define cors package in app.js

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const postgresClient = require("./db/config");
+const cors = require("cors");
 const taskRoute = require("./routes/taskRoute");
+
 const app = express();
 
 //middleware
 app.use(express.json());
+app.use(cors());
 
 // Routes
 app.use("/", taskRoute);
