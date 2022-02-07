1ae07fe1ce67930fa58d49c00fd1ef86d81655eb
mavi <mavi.baris96@gmail.com>
2022-02-07 04:14:16 +0300

    Update

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const postgresClient = require("./db/config");
 const app = express();
 
 //middleware
@@ -6,4 +7,11 @@ app.use(express.json());
 
 app.listen(5000, () => {
   console.log("server has started on port 5000");
+  postgresClient.connect((err) => {
+    if (err) {
+      console.log("connection error", err.stack);
+    } else {
+      console.log("postgres connection successfully");
+    }
+  });
 });
