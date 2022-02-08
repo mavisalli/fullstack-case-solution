c5c59a45ad76072730d9dcf8b8b52eced8c5c354
mavi <mavi.baris96@gmail.com>
2022-02-09 02:53:56 +0300

    Delete imports of junk files

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,8 @@
 import TodoForm from "../src/components/TodoForm";
-import styles from "./styles/todo.module.css";
 
 function App() {
   return (
-    <div className={styles.genel}>
+    <div>
       <TodoForm />
     </div>
   );
