d55324991b75d4ae5ccd945c69c4717240308a0b
mavi <mavi.baris96@gmail.com>
2022-02-13 22:46:00 +0300

    Delete files due to directory change

--- db/config.js
+++
@@ -1,9 +0,0 @@
-const dotenv = require("dotenv");
-const pg = require("pg");
-
-dotenv.config();
-const postgresClient = new pg.Pool({
-  connectionString: process.env.DB_CONNECTION_STRING,
-});
-
-module.exports = postgresClient;
